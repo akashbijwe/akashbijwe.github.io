fb1127da7bbd4a41977edc0a4548494fd1774124
Akash Bijwe <mr.akashbijwe@gmail.com>
2017-06-29 17:44:52 +0530

    adding files to cache

--- sw.js
+++ sw.js
@@ -1,4 +1,20 @@
 console.log("sw.js");
+var cache_name = "service-worker-test-cache";
+var urlToCache = [
+  '/',
+  'bootstrap.min.css',
+  'jquery.min.js',
+  'bootstrap.min.js',
+];
 self.addEventListener('install', function(event){
   console.log("inside install event");
+  
+  event.waitUntil(
+    caches.open(cache_name)
+    .then(function(cache){
+      console.log("inside cache");
+      return cache.addAll(urlToCache);
+    })
+  );
+  
 });
