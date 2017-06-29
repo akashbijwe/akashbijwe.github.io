d96b580fcc9d29c81f811b6e51f6d104a07b0ad1
Akash Bijwe <mr.akashbijwe@gmail.com>
2017-06-29 18:56:02 +0530

    Adding activate eventlistener

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 console.log("sw.js");
-var cache_name = "service-worker-test-cache";
+var cache_name = "service-worker-test-cache-v1";
 var urlToCache = [
   '/',
   'bootstrap.min.css',
@@ -19,6 +19,21 @@ self.addEventListener('install', function(event){
   
   self.addEventListener('activate', function(event){
     console.log("inside activate event");
+    
+    event.waitUntil(
+      caches.keys().then(function(cacheNames){
+        return Promise.all(
+          cacheNames.filter(function(cacheName){
+            // Return true if you want to remove this cache,
+            // but remember that caches are shared across
+            // the whole origin
+          }.map(function(cacheName){
+            return caches.delete(cacheName);
+          })
+         );
+        );
+      });
+    );
   });
   
   self.addEventListener('fetch', function(event){
