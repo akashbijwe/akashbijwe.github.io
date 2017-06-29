330ce96f6e8030075de6f07807082b9d6cc1f09f
Akash Bijwe <mr.akashbijwe@gmail.com>
2017-06-29 19:16:14 +0530

    updating activate function();

--- sw.js
+++ sw.js
@@ -17,23 +17,21 @@ self.addEventListener('install', function(event){
     })
   );
 });
-  self.addEventListener('activate', function(event){
-    console.log("inside activate event");
-    
-    event.waitUntil(
-      caches.keys().then(function(cacheNames){
-        return Promise.all(
-          cacheNames.filter(function(cacheName){
-            // Return true if you want to remove this cache,
-            // but remember that caches are shared across
-            // the whole origin
-          }.map(function(cacheName){
-            return caches.delete(cacheName);
-          })
-        )
-      })
-    );
-  });
+ self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.filter(function(cacheName) {
+          // Return true if you want to remove this cache,
+          // but remember that caches are shared across
+          // the whole origin
+        }).map(function(cacheName) {
+          return caches.delete(cacheName);
+        })
+      );
+    })
+  );
+});
   
   self.addEventListener('fetch', function(event){
     console.log("inside fetch event");
